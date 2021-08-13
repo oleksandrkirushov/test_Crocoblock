e52e2137deb69877a4ba5dd592bbc2153f899f7f
oleksandrkirushov <alex.kirushov@gmail.com>
2021-08-13 05:28:16 +0300

    finished develops test_Crocoblock

---
+++ kirushov/src/js/script.js
@@ -0,0 +1,18 @@
+import $ from "jquery";
+import "slick-carousel";
+
+$(".banner").slick({
+    dots: true,
+    arrows: true,
+    dotsClass: "slick-dots",
+});
+
+$(".book__slider").slick({
+    slidesToShow: 5,
+    arrows: true,
+    dotsClass: "slick-dots",
+    dots: true,
+    slidesToScroll: 2,
+    // centerMode: true,
+    // // centerPadding: "10px",
+});
